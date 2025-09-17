32c121a530055278e17b4c1f1ea4af25d0fa321e
ForgeOrion-DevelopmentTeam <licensing@forgeorion.com>
2025-09-17 17:18:01 +0530

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -16,13 +16,7 @@ export default defineConfig({
       },
     },
     chunkSizeWarningLimit: 1000,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: "esbuild",
   },
   server: {
     headers: {
